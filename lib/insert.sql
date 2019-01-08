85512788e308309ed602e377744564057f2e6a2c
Elyse Burmester <burmesterelyse@gmail.com>
2019-01-08 19:57:02 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2 , "male", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2 , "male", "brown", "rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 1 , "female", "black", "nosey", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 4 , "male", "brown", "stoic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 1 , "female", "white", "energetic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 5 , "male", "brown", "mean", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 3 , "female", "brown", "curious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 2 , "male", "brown", "savage", 0);
