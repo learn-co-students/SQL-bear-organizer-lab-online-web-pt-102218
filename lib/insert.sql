b20885506fae0bb55ab43e89189b66482fe0426a
Paul Ashour <paul.ashour@gmail.com>
2019-01-10 16:12:07 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, temperament) VALUES ("Mr. Chocolate", 1, "blue");
+INSERT INTO bears (name, age, temperament) VALUES ("Rowdy", 3, "peppy");
+INSERT INTO bears (name, age, temperament) VALUES ("Tabitha", 3, "prancing");
+INSERT INTO bears (name, age, temperament) VALUES ("Sergeant Brown", 17, "pardonable");
+INSERT INTO bears (name, age, temperament) VALUES ("Melissa", 2, "eschatologically challenged");
+INSERT INTO bears (name, age, temperament) VALUES ("Grinch", 3, "pickled");
+INSERT INTO bears (name, age, temperament) VALUES ("Wendy", 5, "placid");
+INSERT INTO bears (age, temperament) VALUES (4, "angry");
