4a55a2e7db28766baa9a9865244ebfbc6a8f2eee
Leslie Osorio <lesosorio17@gmail.com>
2019-02-12 20:50:12 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -7,12 +7,3 @@ CREATE TABLE bears (
     temperament TEXT,
     alive BOOLEAN
 );
-
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 3, "male", "brown", "kind", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 5, "male", "light brown", "calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 4, "female", "brown", "sweet", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 7, "male", "light brown", "calm", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 6, "female", "caramel brown", "feisty", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 8, "male", "black", "grumpy", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 8, "female", "black", "sweet", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 2, "male", "dark drown", NULL, 0);
